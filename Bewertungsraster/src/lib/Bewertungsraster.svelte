ef2f33aa53659fef2c4243d6bcdb7034fb8dc22b
Fabian Hirter <mail@fabianhirter.ch>
2025-08-12 12:08:20 +0200

    feat: improve ux

--- Bewertungsraster/src/lib/Bewertungsraster.svelte
+++ Bewertungsraster/src/lib/Bewertungsraster.svelte
@@ -15,36 +15,27 @@
 
     let name = $state("");
 
-    const {form, data} = createForm({
+    let {form, data: formData} = createForm({
         onSubmit: (values) => {
-            render($data)
+            render($formData)
         },
     })
 
     const allBewertungsraster = $state([]);
 
-    loadFromStorage();
+    loadAllBewertungsrasterFromStorage();
 
-    function storeData(bewertungsraster) {
-        window.localStorage.setItem(`bewertungsraster-tel`, JSON.stringify(bewertungsraster));
-    }
-
-    function loadData() {
-        const data = window.localStorage.getItem(`bewertungsraster-tel`);
-        if (data === null) return [];
-        return JSON.parse(data);
-    }
-
-    data.subscribe((data) => {
+    formData.subscribe((data) => {
         // store current dataset in array
         const entry = allBewertungsraster.find((bewertungsraster) => bewertungsraster.name === name);
         if (typeof entry !== "undefined") {
             entry.data = data;
         }
+        // persist
         storeData(allBewertungsraster);
     })
 
-    const punkte = derived(data, (data) => {
+    const punkte = derived(formData, (data) => {
         const dataFields = convertData(data);
         const punkte = sumPunkte(dataFields);
         const note = calculateNote(punkte, max);
@@ -62,51 +53,70 @@
 
     function addStudent(event) {
         event.preventDefault();
+        const inputField = event.target.previousElementSibling;
+        const name = inputField.value;
         allBewertungsraster.push({
             name,
-            data: $data,
+            data: $formData,
         })
         storeData(allBewertungsraster);
+        inputField.value = "";
     }
 
-    function loadFromStorage() {
+    function selectBewertungsraster(event) {
+        const targetName = event.target.value;
+        const bewertungsraster = allBewertungsraster.find((bewertungsraster) => bewertungsraster.name === targetName)
+        name = bewertungsraster.name;
+        $formData = bewertungsraster.data;
+        console.log($formData)
+    }
+
+    function loadAllBewertungsrasterFromStorage() {
         allBewertungsraster.splice(0, allBewertungsraster.length);
 
         const bewertungsrasterFromStorage = loadData();
+
         for (const fromStorage of bewertungsrasterFromStorage) {
             allBewertungsraster.push(fromStorage);
         }
     }
 
-    function loadBewertungsraster(event) {
-        name = event.target.value;
+    function storeData(data) {
+        const storageKey = `bewertungsraster-tel`;
+        window.localStorage.setItem(storageKey, JSON.stringify(data));
+    }
 
-        const entry = allBewertungsraster.find((bewertungsraster) => bewertungsraster.name === name)
-        $data = entry.data;
+    function loadData() {
+        const storageKey = `bewertungsraster-tel`;
+        const data = window.localStorage.getItem(storageKey);
+        if (data === null) return [];
+        return JSON.parse(data);
     }
 </script>
 
-<h1>Bewertungsraster TEL {$data.name}</h1>
+<h1>Bewertungsraster TEL {name}</h1>
 <div>
     <ul>
         <li>Note: <strong>{$punkte.note}</strong></li>
         <li>Punkte: {$punkte.punkte}/{$punkte.max}</li>
     </ul>
 
-    <form>
-        <select name="name" onchange={loadBewertungsraster}>
+    <form class="student">
+        <label for="name">Name</label>
+        <input type="text" id="name" name="name">
+        <button onclick={addStudent}>Hinzufügen</button>
+
+        <label for="student">Student auswählen</label>
+        <select id="student" name="name" onchange={selectBewertungsraster}>
             {#each allBewertungsraster as bewertung}
                 <option>{bewertung.name}</option>
             {/each}
         </select>
-        <button onclick={addStudent}>Hinzufügen</button>
     </form>
 
-    <form use:form>
-        <label for="name">Name</label>
-        <input type="text" id="name" name="name" bind:value={name}>
+    <form class="bewertung" use:form>
         {#each fields as field}
-            <fieldset class="bewertung">
+            <fieldset>
                 <legend>{field.label}</legend>
                 {#each field.inputs as input}
                     <label for="{field.label}-{input.label}">
@@ -118,15 +128,15 @@
                             type="number"
                             min={input.min}
                             max={input.max}
-                            step=1/
-                            value="{$data?.[field.label]?.[input.label]?.punkte}">
+                            step="1"
+                            value="{$formData?.[field.label]?.[input.label]?.punkte}">
                     <label for="{field.label}-{input.label}-kommentar">
                         Kommentar
                     </label>
                     <textarea
                             id="{field.label}-{input.label}-kommentar"
                             name="{field.label}.{input.label}.kommentar"
-                    >{$data?.[field.label]?.[input.label]?.kommentar}</textarea>
+                    >{$formData?.[field.label]?.[input.label]?.kommentar}</textarea>
                 {/each}
             </fieldset>
         {/each}
