12cba4b50bd3ee5e11decfec2855c90da697be0d
fabian hirter <mail@fabianhirter.ch>
2025-11-01 14:25:25 +0100

    test: simplify test

--- Bewertungsraster/tests/bewertungsraster.spec.js
+++ Bewertungsraster/tests/bewertungsraster.spec.js
@@ -1,5 +1,6 @@
 // @ts-check
 import {test, expect} from '@playwright/test';
+import {fields} from "../src/fields.js";
 
 const url = 'http://localhost:5173/TekoBernElektrotechnik/';
 
@@ -25,61 +26,20 @@ test('fill form', async ({page}) => {
     await page.goto(url);
     await page.getByRole('textbox', {name: 'Name'}).fill('John Doe');
     await page.getByRole('button', {name: 'Hinzufügen'}).click();
-    await page.getByRole('spinbutton', {name: 'Vollständigkeit (max. 10)'}).fill('10');
-    await page.locator('[id="Abstract-Vollständigkeit-kommentar"]').fill('test');
-    await page.getByRole('spinbutton', {name: 'Verständlichkeit (max. 5)', exact: true}).fill('5');
-    await page.locator('[id="Abstract-Verständlichkeit-kommentar"]').fill('Test');
-    await page.getByRole('spinbutton', {name: 'Länge (max. 5)'}).fill('5');
-    await page.locator('[id="Abstract-Länge-kommentar"]').fill('Test');
-    await page.getByRole('spinbutton', {name: 'Ausgangslage und'}).fill('10');
-    await page.locator('[id="Einleitung-Ausgangslage und Problemstellung-kommentar"]').fill('Test');
-    await page.getByRole('spinbutton', {name: 'Zielsetzung (max. 10)'}).fill('10');
-    await page.locator('#Einleitung-Zielsetzung-kommentar').fill('Test');
-    await page.getByRole('spinbutton', {name: 'Zeitplanung (max. 5)'}).fill('5');
-    await page.locator('#Einleitung-Zeitplanung-kommentar').fill('Test');
-    await page.getByRole('spinbutton', {name: 'Vorgehen (max. 10)'}).fill('10');
-    await page.locator('#Hauptteil-Vorgehen-kommentar').fill('Test');
-    await page.getByRole('spinbutton', {name: 'Variantenentscheide (max. 10)'}).fill('10');
-    await page.locator('#Hauptteil-Variantenentscheide-kommentar').fill('Test');
-    await page.locator('#Hauptteil-Variantenentscheide-kommentar').press('Tab');
-    await page.getByRole('spinbutton', {name: 'Ergebnisse (max. 10)', exact: true}).fill('10');
-    await page.locator('#Hauptteil-Ergebnisse-kommentar').fill('Test');
-    await page.getByRole('spinbutton', {name: 'Diskussion der Ergebnisse ('}).fill('10');
-    await page.locator('[id="Diskussion-Diskussion der Ergebnisse-kommentar"]').fill('Test');
-    await page.getByRole('spinbutton', {name: 'Eigene Meinung und Reflexion'}).fill('10');
-    await page.getByRole('spinbutton', {name: 'Eigene Meinung und Reflexion'}).press('Tab');
-    await page.locator('[id="Diskussion-Eigene Meinung und Reflexion-kommentar"]').fill('Test');
-    await page.getByRole('spinbutton', {name: 'Empfehlung (max. 5)'}).fill('5');
-    await page.locator('[id="Empfehlung und Ausblick-Empfehlung-kommentar"]').fill('Test');
-    await page.getByRole('spinbutton', {name: 'Ausblick (max. 10)'}).fill('10');
-    await page.locator('[id="Empfehlung und Ausblick-Ausblick-kommentar"]').fill('Test');
-    await page.getByRole('spinbutton', {name: 'Quellen (max. 5)'}).fill('5');
-    await page.locator('[id="Quellen und Hilfsmittel-Quellen-kommentar"]').fill('Test');
-    await page.getByRole('spinbutton', {name: 'Hilfsmittel (max. 5)'}).fill('5');
-    await page.locator('[id="Quellen und Hilfsmittel-Hilfsmittel-kommentar"]').fill('Test');
-    await page.getByRole('spinbutton', {name: 'Code, Schemas, Zeichnungen ('}).fill('10');
-    await page.getByRole('group', {name: 'Anhang'}).getByLabel('Kommentar').fill('Test');
-    await page.getByRole('spinbutton', {name: 'Gliederung, Aufbau, Struktur'}).fill('5');
-    await page.locator('[id="Bericht und Design-Gliederung, Aufbau, Struktur-kommentar"]').fill('Test');
-    await page.getByRole('spinbutton', {name: 'Darstellung (max. 5)'}).fill('5');
-    await page.locator('[id="Bericht und Design-Darstellung-kommentar"]').fill('Test');
-    await page.getByRole('spinbutton', {name: 'Weekly Report (max. 5)'}).fill('5');
-    await page.locator('[id="Arbeitsweise-Weekly Report-kommentar"]').fill('Test');
-    await page.getByRole('spinbutton', {name: 'Einhaltung der Termine (max.'}).fill('5');
-    await page.locator('[id="Arbeitsweise-Einhaltung der Termine-kommentar"]').fill('Test');
-    await page.getByRole('spinbutton', {name: 'Inhaltliche Vollständigkeit'}).fill('5');
-    await page.locator('[id="Präsentation-Inhaltliche Vollständigkeit und Verständlichkeit-kommentar"]').fill('Test');
-    await page.getByRole('spinbutton', {name: 'Beantwortung der Fragen (max'}).fill('10');
-    await page.locator('[id="Präsentation-Beantwortung der Fragen-kommentar"]').fill('Test');
-    await page.getByRole('spinbutton', {name: 'Zeitverwaltung (max. 5)'}).fill('5');
-    await page.locator('[id="Präsentation-Zeitverwaltung-kommentar"]').fill('Test');
-    await page.getByRole('spinbutton', {name: 'Inhalt und Verständlichkeit ('}).fill('5');
-    await page.locator('[id="Onlinepublikation-Inhalt und Verständlichkeit-kommentar"]').fill('Test');
-    await page.getByRole('spinbutton', {name: 'Ausgestaltung (max. 5)'}).fill('5');
-    await page.locator('#Onlinepublikation-Ausgestaltung-kommentar').fill('Test');
-    await page.getByRole('spinbutton', {name: 'Schwierigkeitsgrad (max. 15)'}).fill('0');
-    await page.getByRole('group', {name: 'Bonus'}).getByLabel('Kommentar').fill('Test');
+    await page.getByLabel('Student auswählen').selectOption('John Doe');
+    await page.getByRole('button', { name: 'Laden', exact: true}).click();
+
+    for (const field of fields) {
+        for (const input of field.inputs) {
+              await page
+                  .getByLabel(`${input.label} (max. ${input.max})`,  {exact: true})
+                  .selectOption('tadellos');
+
+              await page.locator(`[id="${field.label}-${input.label}-kommentar"]`).fill('test');
+        }
+    }
+
     await expect(page.getByRole('list')).toContainText('Note: 6');
-    await expect(page.getByRole('list')).toContainText('Punkte: 180/180');
+    await expect(page.getByRole('list')).toContainText('Punkte: 195/180');
 });
 
